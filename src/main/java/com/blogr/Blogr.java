93e24a56a1af338a8b261d25caa329cfb67ee42d
Anadi Mishra <62553697+anadi223@users.noreply.github.com>
2023-10-05 16:52:54 +0530

    Initial Commit

---
+++ src/main/java/com/blogr/Blogr.java
@@ -0,0 +1,13 @@
+package com.blogr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogr {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blogr.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogr/BlogrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
