2a679e69945ac8415b9479d4717d45833c3c794c
Anadi Mishra <62553697+anadi223@users.noreply.github.com>
2023-10-31 10:39:39 +0530

    Swagger Implementation

---
+++ src/main/java/com/blogr/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.blogr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+public class SecurityConfig {
+
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeHttpRequests(configurer -> configurer.requestMatchers(HttpMethod.GET,))
+//    }
+}
